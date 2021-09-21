b9bc9c33176fbafa177111b96c17b7fe9c428abd
leo_Nardo <leo9009am@gmail.com>
2021-09-21 23:12:41 +0500

    fixing v 0.1

--- app/src/main/java/com/leonardo/alifculc/MainFragment.kt
+++ app/src/main/java/com/leonardo/alifculc/MainFragment.kt
@@ -1,19 +1,15 @@
 package com.leonardo.alifculc
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import java.text.DecimalFormat
-import kotlin.math.roundToInt
-import kotlin.math.roundToLong
+
 
 class MainFragment : Fragment(R.layout.fragment_main) {
     lateinit var txtThreeMonth: TextView
@@ -52,66 +48,45 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
         btn = view.findViewById(R.id.btnCount)
 
-        val monthThree = 3
-        val threePercent = 6
-        val monthSix = 6
-        val sixPercent = 10
-        val monthNine = 9
-        val ninePercent = 15
-        val monthTwelve = 12
-        val twelvePercent = 20
-        val monthFifteen = 15
-        val fifteenPercent = 24
-        val monthEighteen = 18
-        val eighteenPercent = 27
-        val monthTwentyFour = 24
-        val twentyFourPercent = 30
-
-
-
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
-        viewModel.name.observe(viewLifecycleOwner){
-            txtThreeMonth.text = it.toString()
-        }
 
         btn.setOnClickListener {
             val getEdit = editTxt.text.toString()
 
             if(getEdit.isEmpty()){
-                Toast.makeText(requireContext(), "puy number", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "put number", Toast.LENGTH_SHORT).show()
             }else{
                 val inputD: Double = getEdit.toDouble()
 
-                val sumThree = (((inputD * threePercent)/100) + inputD)/monthThree
-                val sumThreeResult = String.format("%.2f", sumThree).toDouble()
-                txtThreeMonth.text = sumThreeResult.toString()
+                val sumThree = (((inputD * 6)/100) + inputD)/3
+                val sumThreeResult = String.format("%.2f", sumThree)
+                txtThreeMonth.text = sumThreeResult
 
-//fklfsdk
+                val sumSix = (((inputD * 10)/100) + inputD)/6
+                val sumSixResult = String.format("%.2f", sumSix)
+                txtSixMonth.text = sumSixResult
 
-                val sumSix = (((inputD * sixPercent)/100) + inputD)/monthSix
-                val sumSixResult = String.format("%.2f", sumSix).toDouble()
-                txtSixMonth.text = sumSixResult.toString()
+                val sumNine = (((inputD * 15)/100) + inputD)/9
+                val sumNineResult = String.format("%.2f", sumNine)
+                txtNineMonth.text = sumNineResult
 
-                val sumNine = (((inputD * ninePercent)/100) + inputD)/monthNine
-                val sumNineResult = String.format("%.2f", sumNine).toDouble()
-                txtNineMonth.text = sumNineResult.toString()
+                val sumTwelve = (((inputD * 20)/100) + inputD)/12
+                val sumTwelveResult = String.format("%.2f", sumTwelve)
+                txtTwelveMonth.text = sumTwelveResult
 
-                val sumTwelve = (((inputD * twelvePercent)/100) + inputD)/monthTwelve
-                val sumTwelveResult = String.format("%.2f", sumTwelve).toDouble()
-                txtTwelveMonth.text = sumTwelveResult.toString()
+                val sumFifteen = (((inputD * 24)/100) + inputD)/15
+                val sumFifteenResult = String.format("%.2f", sumFifteen)
+                txtFifteenMonth.text = sumFifteenResult
 
-                val sumFifteen = (((inputD * fifteenPercent)/100) + inputD)/monthFifteen
-                val sumFifteenResult = String.format("%.2f", sumFifteen).toDouble()
-                txtFifteenMonth.text = sumFifteenResult.toString()
+                val sumEighteen = (((inputD * 27)/100) + inputD)/18
+                val sumEighteenResult = String.format("%.2f", sumEighteen)
+                txtEighteenMonth.text = sumEighteenResult
 
-                val sumEighteen = (((inputD * eighteenPercent)/100) + inputD)/monthEighteen
-                val sumEighteenResult = String.format("%.2f", sumEighteen).toDouble()
-                txtEighteenMonth.text = sumEighteenResult.toString()
+                val sumTwentyFour = (((inputD * 30)/100) + inputD)/24
+                val sumTwentyFourResult = String.format("%.2f", sumTwentyFour)
+                txtTwentyFourMonth.text = sumTwentyFourResult
 
-                val sumTwentyFour = (((inputD * twentyFourPercent)/100) + inputD)/monthTwentyFour
-                val sumTwentyFourResult = String.format("%.2f", sumTwentyFour).toDouble()
-                txtTwentyFourMonth.text = sumTwentyFourResult.toString()
+                //fox
 
             }
 
