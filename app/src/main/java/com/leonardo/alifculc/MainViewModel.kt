f5b30a5fd16d09ce5edcf59e6a31f109d2ae2121
leo_Nardo <leo9009am@gmail.com>
2021-09-29 00:29:04 +0500

    fixing v 1.3

--- app/src/main/java/com/leonardo/alifculc/MainViewModel.kt
+++ app/src/main/java/com/leonardo/alifculc/MainViewModel.kt
@@ -5,5 +5,10 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
     val txtThree = MutableLiveData<Double>()
-
+    val txtSix = MutableLiveData<Double>()
+    val txtNine = MutableLiveData<Double>()
+    val txtTwelve = MutableLiveData<Double>()
+    val txtFifteen = MutableLiveData<Double>()
+    val txtEighteen = MutableLiveData<Double>()
+    val txtTwentyFour = MutableLiveData<Double>()
 }
